6d25406521d0141589e8c0bbc2e2528d876e7332
sem-nome <matheusfoliveira172@gmail.com>
2022-04-12 20:10:37 -0300

    frontend: equipamento

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const route = express.Router();
 const loginController = require('./src/controllers/loginController');
+const computador = require('./src/controllers/computadorController')
 
 
 //pagina de home
@@ -8,5 +9,11 @@ route.get('/', loginController.index);
 
 //pagina de logado
 route.get('/logado', loginController.logado);
+
+//pagina de cadastro
+route.get('/cadastrocomputador', computador.cadastro);
+route.get('/buscacomputador', computador.busca);
+route.get('/listagemcomputador', computador.listagem);
+route.get('/cadastrodeequipamento', computador.cadastroDePc);
 
 module.exports = route;
---
+++ src/controllers/computadorController.js
@@ -0,0 +1,14 @@
+exports.cadastro = (req, res) => {
+    res.render('cadastroComputador');
+}
+
+exports.busca = (req, res) => {
+    res.render('buscaComputador');
+}
+exports.listagem = (req, res) => {
+    res.render('listagemTombo');
+}
+
+exports.cadastroDePc = (req, res) => {
+    res.render('servicoComputador');
+}
